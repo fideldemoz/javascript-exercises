5fe6ae6e72afd80b9394901154a435a748202521
Fidel Livino Pedro <fidellplivino98@gmail.com>
2023-05-12 20:51:26 +0200

    Solve Calculator and palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+	if (arr.length == 0) {
+		return 0;
+	}
+	arr.forEach((number) => {
+		sum += number;
+	});
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let temp = 1;
+	arr.forEach((number) => {
+		temp *= number;
+	});
+	return temp;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let factorial = n;
+	if (n === 0) {
+	return 1;
+	}
+	for (let i = n-1; i>0; i--) {
+		factorial *= i;
+	}
+	return factorial
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,40 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	str = str.toLowerCase()
+	strLength = str.length;
+	strLastIndex = strLength - 1;
+	
+	if (str[strLastIndex] === '.' || str[strLastIndex] === '!' || str[strLastIndex] === '?') {
+		str = str.slice(0,strLastIndex)
+	}
+	newLenght = str.length;
+	newLI = newLenght -1;
+	counter = 0;
+	strStore = []
+	newStr = []
+	while (counter <= newLI) {
+		let char = str[counter];
+		if ( char !== ' ' && char !== ',') {
+			strStore.push(char);
+		}
+		counter++;
+	}
+	
+	while (newLI >=0) {
+		let char = str[newLI];
+		if ( char !== ' ' && char !== ',') {
+			newStr.push(char);
+		}
+		newLI--;
+	}
+	
+	original = strStore.join('')
+	palindrome = newStr.join('')
 
+	if (original === palindrome) {
+		return true;
+	} else {
+		return false
+	}
 };
 
 // Do not edit below this line
