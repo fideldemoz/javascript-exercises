eb33c3aecaf632d6a5e385dc753d78fe557b4632
Fidel Livino Pedro <fidellplivino98@gmail.com>
2023-05-13 12:10:48 +0200

    Finish Exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	let Phi = (1 + Math.sqrt(5)) / 2
+	let phi = (1 - Math.sqrt(5)) / 2
+	return n < 0 ? "OOPS" : parseInt((Phi ** n - phi ** n) / Math.sqrt(5));
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+	return arr.reduce((first, current) => {
+		const oldestAge = getAge(first.yearOfBirth, first.yearOfDeath)
+		const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+		
+		return oldestAge < currentAge ? current : first;
+	})
 };
 
+const getAge = (birth, death) => { if (!death) {death = new Date().getFullYear();} return death - birth}
 // Do not edit below this line
 module.exports = findTheOldest;
